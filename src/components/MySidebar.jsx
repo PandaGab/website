f26228b3c3bb9a33672dac024e391a51154250d8
PandaGab <gabriel.leclerc.2@ulaval.ca>
2022-09-06 07:08:50 -0400

    🎉 Remove all elements from the template

--- src/components/MySidebar.jsx
+++
@@ -1,9 +0,0 @@
-export default function MySidebar(props) {
-    return (
-      <aside>
-        <header>{props.title}</header>
-        <main>{props.children}</main>
-        <footer>{props.socialLinks}</footer>
-      </aside>
-    )
-  }
